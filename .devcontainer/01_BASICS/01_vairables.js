61896b01fef46d38149cc6839f95d44cf5f72763
amant8183 <129760431+amant8183@users.noreply.github.com>
2023-10-23 10:37:33 +0000

    container config done

---
+++ .devcontainer/01_BASICS/01_vairables.js
@@ -0,0 +1,11 @@
+const accountId = 144553
+let accountEmail ="amat8183@gmail.com"
+var accPass = "12345"
+accCity ="Jaipur"
+
+let accountState
+// accountId = 2
+accountEmail ="ok@gmail.com"
+accPass = "2351"
+accCity="main"
+console.table([accPass,accountEmail,accountId,accCity,accountState])
---
+++ .devcontainer/01_BASICS/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict";//treat all JS code as newer version
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3+3);
+console.log("Aman");
+
+let name = "Aman";
+let age = 18;
+let isloggedIn = false;
+
+
+// number = 2 to power 53
+// BigInt
+// String=> ""
+// Boolean=>true/false
+// null => standalone value
+// undefined =>
+// symbol
+
+// object
+
+console.log(typeof null)
+console.log(typeof undefined);
